00a9b8e4a0319f0106e339a4cf9ed70e24de44cc
Bora Odabasi <boraodabasi@Boras-MacBook-Pro.local>
2020-11-12 15:46:02 -0500

    project changes message

--- dark_and_stormy.R
+++ dark_and_stormy.R
@@ -1,5 +1,4 @@
 
-print("It was a dark and stormy night; the rain fell in torrents.")
-
+print("It was a dark and stormy night so I stayed in to complete my R project and contemplate the future.")
 
 
